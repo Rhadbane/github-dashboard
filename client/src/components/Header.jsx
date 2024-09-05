939b75577d7a888366de4553ca9df65aa5cfdec8
Rhadbane <rhadbane.idriss@gmail.com>
2024-09-05 20:00:50 +0200

    new

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
-import { Navbar } from "flowbite-react";
+import { Button, Navbar } from "flowbite-react";
 import { Link } from "react-router-dom";
 import { TextInput } from 'flowbite-react';
-import {AiOutlineSearch} from 'react-icons/ai';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from 'react-icons/fa';
+
 export default function Header() {
   return (
     <Navbar className='border-b-2'>
@@ -18,6 +20,29 @@ export default function Header() {
         />
         <AiOutlineSearch className='ml-2 text-lg text-gray-500' />
       </form>
+
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color='gray' pill>
+          <FaMoon />
+        </Button>
+        <Link to='/sign-in'>
+          <Button gradientDuoTone='purpleToBlue'>
+            SignIn
+          </Button>
+        </Link>
+      </div>
+
+      <Navbar.Collapse>
+        <Link to='/' className='block py-2 px-4 text-sm font-medium text-gray-700 dark:text-gray-400'>
+          Home
+        </Link>
+        <Link to='/about' className='block py-2 px-4 text-sm font-medium text-gray-700 dark:text-gray-400'>
+          About
+        </Link>
+        <Link to='/projects' className='block py-2 px-4 text-sm font-medium text-gray-700 dark:text-gray-400'>
+          Projects
+        </Link>
+      </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
